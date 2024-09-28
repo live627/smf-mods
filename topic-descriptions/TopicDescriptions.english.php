2a036ac3183c2a0896eac0c349ed7fd3fac5c989
GL700Wing <67716760+GL700Wing@users.noreply.github.com>
2024-09-28 17:46:21 +1000

    Update TopicDescriptions.english.php (#5)
    Added text strings for new option to choose description placement.

--- topic-descriptions/TopicDescriptions.english.php
+++ topic-descriptions/TopicDescriptions.english.php
@@ -8,3 +8,6 @@ $txt['topic_descriptions_enable'] = 'Enable Topic Descriptions?';
 $txt['topic_descriptions_post_desc'] = 'Description (<i>Optional</i>):';
 $txt['select_boards_from_list'] = 'Select boards which apply';
 $txt['topic_descriptions_topics'] = 'Enable the description in the topic?';
+$txt['topic_descriptions_where'] = 'Location for description on message index';
+$txt['topic_descriptions_below'] = 'Below the topic author name';
+$txt['topic_descriptions_right'] = 'To the right of the topic title';
