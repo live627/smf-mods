f8473753654c4315af0ba819ee36873050cbdeeb
John Rayes <live627@gmail.com>
2025-03-11 22:27:10 -0700

    Update CountdownBBC.english.php

--- countdown/CountdownBBC.english.php
+++ countdown/CountdownBBC.english.php
@@ -1,6 +1,6 @@
 <?php
 
-$txt['countdown'] = 'Countdown (Example [countdown=8,14,2009,13,23]Text[/countdown])';
+$txt['countdown'] = 'Countdown';
 
 /*
 $txt['cd_year']   = '{count, plural, one {# year} other {# years}}';
@@ -10,10 +10,10 @@ $txt['cd_hour']   = '{count, plural, one {# hour} other {# hours}}';
 $txt['cd_minute'] = '{count, plural, one {# minute} other {# minutes}}';
 */
 
-$txt['cd_year']   = '# year|# years';  // For "1 year" or "2 years"
-$txt['cd_month']  = '# month|# months';  // For "1 month" or "2 months"
-$txt['cd_day']    = '# day|# days';  // For "1 day" or "2 days"
-$txt['cd_hour']   = '# hour|# hours';  // For "1 hour" or "2 hours"
+$txt['cd_year']   = '# year|# years';      // For "1 year" or "2 years"
+$txt['cd_month']  = '# month|# months';    // For "1 month" or "2 months"
+$txt['cd_day']    = '# day|# days';        // For "1 day" or "2 days"
+$txt['cd_hour']   = '# hour|# hours';      // For "1 hour" or "2 hours"
 $txt['cd_minute'] = '# minute|# minutes';  // For "1 minute" or "2 minutes"
 
-$txt['cd_remaining'] = ' remaining.';
+$txt['cd_remaining'] = '{0} remaining.';
