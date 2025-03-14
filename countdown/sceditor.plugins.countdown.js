3cdde366b6d0742c730abb4f13b342ce40e38d91
John Rayes <live627@gmail.com>
2025-03-14 02:31:09 -0700

    Update sceditor.plugins.countdown.js

--- countdown/sceditor.plugins.countdown.js
+++ countdown/sceditor.plugins.countdown.js
@@ -3,7 +3,7 @@
 
 	sceditor.plugins.countdown = function () {
 		this.init = function () {
-				const opts = this.opts;
+			const opts = this.opts;
 
 			const commandHandler = function (caller) {
 				const editor = this;
@@ -12,20 +12,31 @@
 				const data = [
 					['MM', 'DD', 'YYYY'],
 					['HH', 'MM'],
+					[editor._('Description')],
 				];
 
 				for (let i = 0; i < data.length; i++) {
 					const div = document.createElement('div');
 
 					for (let j = 0; j < data[i].length; j++) {
+						const id = 'cd-inp-' + (i + j);
+						const label = document.createElement('label');
+						label.setAttribute('for', id);
+						label.textContent = data[i][j];
 						const input = document.createElement('input');
 						input.type = 'text';
 						input.dir = 'ltr';
-						input.size = data[i][j].length;
-						input.required = i === 0;
-						input.maxLength = data[i][j].length;
-						input.pattern = '\\d{1,' + data[i][j].length + '}';
-						div.append(input, data[i][j]);
+						input.id = id;
+						input.required = i === 0 || i === 2;
+
+						if (i < 2) {
+							const length = data[i][j].length;
+							input.size = length;
+							input.maxLength = length;
+							input.pattern = '\\d{1,' + length + '}';
+						}
+
+						div.append(label, input);
 					}
 
 					content.appendChild(div);
@@ -44,13 +55,16 @@
 					let params = [];
 
 					for (let el of this.elements) {
-						if (el.value && el.type !== 'submit') {
+						if (el.value && el.size != 20) {
 							params.push(el.value);
 						}
 					}
 
 					if (params.length && this.checkValidity()) {
-						editor.insertText('[countdown=' + params.join(',') + ']', '[/countdown]');
+						editor.insert(
+							'[countdown=' + params.join(',') + ']' + this.elements[5].value,
+							'[/countdown]'
+						);
 					}
 
 					editor.closeDropDown(true);
