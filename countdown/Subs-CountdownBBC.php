b041ae927670ff62f90574af9deec6367f92ed67
John Rayes <live627@gmail.com>
2025-03-14 02:27:46 -0700

    Update Subs-CountdownBBC.php

--- countdown/Subs-CountdownBBC.php
+++ countdown/Subs-CountdownBBC.php
@@ -14,6 +14,9 @@ class CountdownBBC
 			{
 				global $txt;
 
+				if (isset($disabled['countdown']))
+					return;
+
 				$end_time = mktime(
 					empty($data[4]) ? 0 : intval($data[4]), // Hour
 					empty($data[5]) ? 0 : intval($data[5]), // Minute
@@ -26,24 +29,24 @@ class CountdownBBC
 				$remain_time = $end_time - time();
 				$parts = [];
 
-				if ($remain_time > 0) {
-					// Time units with their corresponding translation keys
-					$time_units = [
-						31556926 => 'cd_year', // 1 year in seconds
-						2628288 => 'cd_month', // 1 month in seconds
-						86400 => 'cd_day',     // 1 day in seconds
-						3600 => 'cd_hour',     // 1 hour in seconds
-						60 => 'cd_minute',     // 1 minute in seconds
-					];
+				// Time units with their corresponding translation keys
+				$time_units = [
+					31556926 => 'cd_year', // 1 year in seconds
+					2628288 => 'cd_month', // 1 month in seconds
+					86400 => 'cd_day',     // 1 day in seconds
+					3600 => 'cd_hour',     // 1 hour in seconds
+					60 => 'cd_minute',     // 1 minute in seconds
+				];
 
-					foreach ($time_units as $unit_seconds => $key) {
-						if ($remain_time >= $unit_seconds) {
-							$value = intval($remain_time / $unit_seconds);
-							$remain_time -= $value * $unit_seconds;
-							$parts[] = self::format_time_unit($value, $key);
-						}
+				foreach ($time_units as $unit_seconds => $key) {
+					if ($remain_time >= $unit_seconds) {
+						$value = intval($remain_time / $unit_seconds);
+						$remain_time -= $value * $unit_seconds;
+						$parts[] = self::format_time_unit($value, $key);
 					}
+				}
 
+				if ($data[0] == '' || $remain_time > 0) {
 					$data = [strtr($txt['cd_remaining'], ['{0}' => sentence_list($parts)])];
 				}
 			},
@@ -64,17 +67,28 @@ class CountdownBBC
 
 	public static function sce_options(array &$sce_options): void
 	{
-		$sce_options['plugins'] = ($sce_options['plugins'] != '' ? $sce_options['plugins'] . ',' : '') . 'countdown';
+		if (allowedTo('bbc_countdown'))
+			$sce_options['plugins'] = ($sce_options['plugins'] != '' ? $sce_options['plugins'] . ',' : '') . 'countdown';
 	}
 
 	public static function bbc_buttons(array &$buttons): void
 	{
+		if (!allowedTo('bbc_countdown'))
+			return;
+
 		global $txt;
 
 		loadCSSFile('countdown.css');
 		loadJavaScriptFile('sceditor.plugins.countdown.js', array('minimize' => true));
-
 		loadLanguage('CountdownBBC');
+		addInlineJavaScript(
+			'
+			sceditor.locale["' . $txt['lang_dictionary'] . '"] = {
+				"Description": "' . $txt['cd_description'] . '",
+			};',
+			true,
+		);
+
 		$temp = array();
 
 		foreach ($buttons[0] as $tag)
